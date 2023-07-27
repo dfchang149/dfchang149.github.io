e9566dee7d30753292e85ec691d9e10e68d29c24
Dustin <dfchang@ucsd.edu>
2023-07-27 15:32:48 -0700

    Added a projectLabel + implemented system theme (need to improve later)

--- changeTheme.js
+++ changeTheme.js
@@ -1,23 +1,39 @@
 const themeLink = document.querySelector("#themeLink");
-const currentTheme = localStorage.getItem("theme");
+const currentTheme = localStorage.getItem("theme") || "dark";
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-if (currentTheme == null) {
-    themeLink.href = "darkTheme.css";
-} else {
-    themeLink.href = currentTheme+"Theme.css";
+const darkThemeIcon = document.getElementById("darkThemeIcon");
+const lightThemeIcon = document.getElementById("lightThemeIcon");
+const systemThemeIcon = document.getElementById("systemThemeIcon");
+
+function loadSystemTheme(){
+    // if(prefersDarkScheme.matches) {
+    //     themeLink.href = "darkTheme.css";
+    // } else {
+    //     themeLink.href = "lightTheme.css";
+    // }
+    themeLink.href = "systemTheme.css";
 }
 
 function swapTheme() {
-    if (themeLink.getAttribute("href") == "lightTheme.css") {
-        themeLink.href = "darkTheme.css";
-        localStorage.setItem("theme","dark");
-    } else {
+    if (themeLink.getAttribute("href") == "darkTheme.css") {
         themeLink.href = "lightTheme.css";
         localStorage.setItem("theme","light");
+    } else if (themeLink.getAttribute("href") == "lightTheme.css") {
+        loadSystemTheme();
+        localStorage.setItem("theme","system");
+    } else {
+        themeLink.href = "darkTheme.css";
+        localStorage.setItem("theme","dark");
     }
 }
 
 function themeButtonPressed() {
     swapTheme();
+}
+
+if(currentTheme == "system") {
+    loadSystemTheme();
+} else {
+    themeLink.href = currentTheme+"Theme.css";
 }
