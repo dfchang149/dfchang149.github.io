815d2d8f2004b7e0cfda463bffe9f0d28f706979
Dustin <dfchang@ucsd.edu>
2023-08-01 08:47:02 -0700

    Trying out a code linter called standard JS

--- changeTheme.js
+++ changeTheme.js
@@ -1,39 +1,22 @@
-const themeLink = document.querySelector("#themeLink");
-const currentTheme = localStorage.getItem("theme") || "dark";
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const themeLink = document.querySelector('#themeLink')
+const currentTheme = localStorage.getItem('theme') || 'dark'
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
 
-const darkThemeIcon = document.getElementById("darkThemeIcon");
-const lightThemeIcon = document.getElementById("lightThemeIcon");
-const systemThemeIcon = document.getElementById("systemThemeIcon");
-
-function loadSystemTheme(){
-    // if(prefersDarkScheme.matches) {
-    //     themeLink.href = "darkTheme.css";
-    // } else {
-    //     themeLink.href = "lightTheme.css";
-    // }
-    themeLink.href = "systemTheme.css";
+function swapTheme () {
+  if (themeLink.getAttribute('href') == 'darkTheme.css') {
+    themeLink.href = 'lightTheme.css'
+    localStorage.setItem('theme', 'light')
+  } else if (themeLink.getAttribute('href') == 'lightTheme.css') {
+    themeLink.href = 'systemTheme.css'
+    localStorage.setItem('theme', 'system')
+  } else {
+    themeLink.href = 'darkTheme.css'
+    localStorage.setItem('theme', 'dark')
+  }
 }
 
-function swapTheme() {
-    if (themeLink.getAttribute("href") == "darkTheme.css") {
-        themeLink.href = "lightTheme.css";
-        localStorage.setItem("theme","light");
-    } else if (themeLink.getAttribute("href") == "lightTheme.css") {
-        loadSystemTheme();
-        localStorage.setItem("theme","system");
-    } else {
-        themeLink.href = "darkTheme.css";
-        localStorage.setItem("theme","dark");
-    }
+function themeButtonPressed () {
+  swapTheme()
 }
 
-function themeButtonPressed() {
-    swapTheme();
-}
-
-if(currentTheme == "system") {
-    loadSystemTheme();
-} else {
-    themeLink.href = currentTheme+"Theme.css";
-}
+themeLink.href = currentTheme + 'Theme.css'
--- scrollToTop.js
+++ scrollToTop.js
@@ -1,94 +1,94 @@
 // Get the button:
-let button = document.getElementById("currentPageButton");
-let goTopLabel = document.getElementById("goTop");
-let pageButtons = document.getElementById("pageButtons");
-let body = document.body;
-let docElement =  document.documentElement;
-let scrollBound = 50
-let isOverPageButtons = false;
+const button = document.getElementById('currentPageButton')
+const goTopLabel = document.getElementById('goTop')
+const pageButtons = document.getElementById('pageButtons')
+const body = document.body
+const docElement = document.documentElement
+const scrollBound = 50
+let isOverPageButtons = false
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() }
 
-function mouseDownCurrPageButton() {
-    if(goTopLabel.style.opacity > 0) {
-        goTopLabel.style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s";
-        goTopLabel.style.padding = "6px 20px";
-        goTopLabel.style.transform = "scale(1) translateY(8px)";
-    }
+function mouseDownCurrPageButton () {
+  if (goTopLabel.style.opacity > 0) {
+    goTopLabel.style.transition = 'visibility 0s ease-in-out 0.5s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s'
+    goTopLabel.style.padding = '6px 20px'
+    goTopLabel.style.transform = 'scale(1) translateY(8px)'
+  }
 }
 
-function mouseOverCurrPageButton() {
-    if(body.scrollTop != 0 || docElement.scrollTop != 0) {
-        goTopLabel.style.visibility = "visible";
-        goTopLabel.style.opacity = 1;
-        goTopLabel.style.transitionDelay = "0.5s";
-        goTopLabel.style.padding = "8px 16px";
-        goTopLabel.style.transform = "scale(1) translateY(0%)";
-    }
+function mouseOverCurrPageButton () {
+  if (body.scrollTop != 0 || docElement.scrollTop != 0) {
+    goTopLabel.style.visibility = 'visible'
+    goTopLabel.style.opacity = 1
+    goTopLabel.style.transitionDelay = '0.5s'
+    goTopLabel.style.padding = '8px 16px'
+    goTopLabel.style.transform = 'scale(1) translateY(0%)'
+  }
 }
 
-function mouseLeaveCurrPageButton(bypass) {
-    if(goTopLabel.style.opacity > 0 || bypass == true) {
-        goTopLabel.style.transition = "visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s";
-        goTopDisappear(bypass);
-    }
+function mouseLeaveCurrPageButton (bypass) {
+  if (goTopLabel.style.opacity > 0 || bypass == true) {
+    goTopLabel.style.transition = 'visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s'
+    goTopDisappear(bypass)
+  }
 }
 
-function goTopDisappear(bypass) {
-    if(goTopLabel.style.opacity > 0 || bypass == true) {
-        goTopLabel.style.opacity = 0;
-        goTopLabel.style.padding = "8px 16px";
-        goTopLabel.style.transform = "scale(0.8) translateY(-30%)"; 
-        goTopLabel.style.visibility = "hidden";
-    }
+function goTopDisappear (bypass) {
+  if (goTopLabel.style.opacity > 0 || bypass == true) {
+    goTopLabel.style.opacity = 0
+    goTopLabel.style.padding = '8px 16px'
+    goTopLabel.style.transform = 'scale(0.8) translateY(-30%)'
+    goTopLabel.style.visibility = 'hidden'
+  }
 }
 
-function mouseOverPageButtons() {
-    if(body.scrollTop <= scrollBound && docElement.scrollTop <= scrollBound){
-        pageButtons.style.backgroundColor = "var(--bar-hover)";
-        pageButtons.style.border = "#7db1ff08 solid 1px";
-    }
-    isOverPageButtons = true;
-    pageButtons.style.transform ="translateY(4px)";
+function mouseOverPageButtons () {
+  if (body.scrollTop <= scrollBound && docElement.scrollTop <= scrollBound) {
+    pageButtons.style.backgroundColor = 'var(--bar-hover)'
+    pageButtons.style.border = '#7db1ff08 solid 1px'
+  }
+  isOverPageButtons = true
+  pageButtons.style.transform = 'translateY(4px)'
 }
 
-function mouseLeavePageButtons() {
-    if(body.scrollTop <= scrollBound && docElement.scrollTop <= scrollBound){
-        pageButtons.style.backgroundColor = "var(--bar-hidden)";
-        pageButtons.style.border = "#7db1ff00 solid 0px";
-    }
-    isOverPageButtons = false;
-    pageButtons.style.transform ="translateY(0px)";
+function mouseLeavePageButtons () {
+  if (body.scrollTop <= scrollBound && docElement.scrollTop <= scrollBound) {
+    pageButtons.style.backgroundColor = 'var(--bar-hidden)'
+    pageButtons.style.border = '#7db1ff00 solid 0px'
+  }
+  isOverPageButtons = false
+  pageButtons.style.transform = 'translateY(0px)'
 }
 
-function scrollFunction() {
-    if (body.scrollTop > scrollBound || docElement.scrollTop > scrollBound) {
-        pageButtons.style.backgroundColor = "var(--bar)";
-        pageButtons.style.boxShadow = "0px 4px 8px var(--purple-shade)";
-        pageButtons.style.border = "#7db1ff15 solid 1px";
+function scrollFunction () {
+  if (body.scrollTop > scrollBound || docElement.scrollTop > scrollBound) {
+    pageButtons.style.backgroundColor = 'var(--bar)'
+    pageButtons.style.boxShadow = '0px 4px 8px var(--purple-shade)'
+    pageButtons.style.border = '#7db1ff15 solid 1px'
+  } else {
+    goTopLabel.style.transition = 'visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s'
+    mouseLeaveCurrPageButton()
+
+    if (isOverPageButtons) {
+      mouseOverPageButtons()
     } else {
-        goTopLabel.style.transition = "visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s";
-        mouseLeaveCurrPageButton();
-
-        if (isOverPageButtons) {
-            mouseOverPageButtons();
-        } else {
-            pageButtons.style.backgroundColor = "var(--bar-hidden)";
-            pageButtons.style.border = "#7db1ff00 solid 0px";
-        }
-        pageButtons.style.boxShadow = "0px 4px 12px var(--purple-shade-hidden)";
+      pageButtons.style.backgroundColor = 'var(--bar-hidden)'
+      pageButtons.style.border = '#7db1ff00 solid 0px'
     }
+    pageButtons.style.boxShadow = '0px 4px 12px var(--purple-shade-hidden)'
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    goTopDisappear();
-    body.scrollTop = 0; // For Safari
-    docElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function topFunction () {
+  goTopDisappear()
+  body.scrollTop = 0 // For Safari
+  docElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
 
-goTopLabel.style.transition = "visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s";
-mouseLeaveCurrPageButton(true);
+goTopLabel.style.transition = 'visibility 0s ease-in-out 1s, opacity 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, padding 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s, transform 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s'
+mouseLeaveCurrPageButton(true)
 
-scrollFunction();
+scrollFunction()
